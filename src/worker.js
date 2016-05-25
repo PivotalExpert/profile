3c2f80150b518ca96480f0299cb1287d36944807
Chris Boesch <cboesch@smu.edu.sg>
2016-05-25 17:01:26 +0800

    Adding async test example

--- src/worker.js
+++ src/worker.js
@@ -3,65 +3,58 @@ var Queue = require('firebase-queue'),
     http = require('http'),
     request = require('request');
 
-var say_hello = function (phrase) {
-    return "Hello "+phrase;
-};
-var say_goodbye = function (phrase) {
-    return "Goodbye "+phrase;
-};
-
-var get_service_url = function(service, serviceID){
-    var theUrl = "";
-    console.log("Fetching serviceID "+serviceID+" on "+service);
-    if (service == "freeCodeCamp") {
-      theUrl = "https://www.freecodecamp.com/" + serviceID;
-      //console.log("Using freeCodeCamp url " + theUrl);
-    }
-    else if (service == "codeCombat") {
-      //console.log("Using codeCombat url");
-      theUrl = "http://codecombat.com/db/user/" + serviceID + "/level.sessions?project=state.complete,levelID,levelName";
-    }
-    else if (service == "codeSchool") {
-      //console.log("Using codeSchool url");
-      theUrl = "https://www.codeschool.com/users/" + serviceID + ".json";
-    }
-    return theUrl;
+var get_service_url = function (service, serviceID) {
+  var theUrl = "";
+  //console.log("Fetching serviceID "+serviceID+" on "+service);
+  if (service == "freeCodeCamp") {
+    theUrl = "https://www.freecodecamp.com/" + serviceID;
+    //console.log("Using freeCodeCamp url " + theUrl);
+  }
+  else if (service == "codeCombat") {
+    //console.log("Using codeCombat url");
+    theUrl = "http://codecombat.com/db/user/" + serviceID + "/level.sessions?project=state.complete,levelID,levelName";
+  }
+  else if (service == "codeSchool") {
+    //console.log("Using codeSchool url");
+    theUrl = "https://www.codeschool.com/users/" + serviceID + ".json";
+  }
+  return theUrl;
 }
 
-var get_achievements_from_response = function(service, body){
-            var totalAchievements = 0;
-            if (service == "freeCodeCamp") {
-              var start = body.indexOf(">[ ");
-              var stop = body.indexOf(" ]<");
-              totalAchievements = body.substring(start + 3, stop);
-            }
-            else if (service == "codeCombat") {
+var get_achievements_from_response = function (service, body) {
+  var totalAchievements = 0;
+  if (service == "freeCodeCamp") {
+    var start = body.indexOf(">[ ");
+    var stop = body.indexOf(" ]<");
+    totalAchievements = body.substring(start + 3, stop);
+  }
+  else if (service == "codeCombat") {
 
-              var jsonObject = JSON.parse(body);
-              //Currently includes stat.complete.false levels
-              console.log("Code Combat levels = " + jsonObject.length);
-              var theCount = 0;
-              for (var i = 0; i < jsonObject.length; i++) {
-                if (jsonObject[i].state.complete == true) {
-                  theCount += 1;
-                }
+    var jsonObject = JSON.parse(body);
+    //Currently includes stat.complete.false levels
+    //console.log("Code Combat levels = " + jsonObject.length);
+    var theCount = 0;
+    for (var i = 0; i < jsonObject.length; i++) {
+      if (jsonObject[i].state.complete == true) {
+        theCount += 1;
+      }
 
-              }
-              console.log("Completed Code Combat levels = " + theCount);
-              totalAchievements = theCount;
-            }
+    }
+    //console.log("Completed Code Combat levels = " + theCount);
+    totalAchievements = theCount;
+  }
 
-            else if (service == "codeSchool") {
-              var jsonObject = JSON.parse(body);
-              var badges = jsonObject['badges'];
-              console.log("Code School Badges earned " + badges.length);
-              totalAchievements = badges.length;
+  else if (service == "codeSchool") {
+    var jsonObject = JSON.parse(body);
+    var badges = jsonObject['badges'];
+    //console.log("Code School Badges earned " + badges.length);
+    totalAchievements = badges.length;
 
-            }
+  }
 
-            console.log("Fetched totalAchievements " + totalAchievements); // Show the HTML for homepage.
-            return totalAchievements;
-  
+  //console.log("Fetched totalAchievements " + totalAchievements+ " on "+service); // Show the HTML for homepage.
+  return totalAchievements;
+
 }
 var ref = new Firebase('https://verifier.firebaseio.com');
 var queueRef = new Firebase('https://verifier.firebaseio.com/queue');
@@ -76,80 +69,94 @@ ref.authWithCustomToken('<YOUR_TOKEN_HERE>', function(err, authData){
 });
 */
 
+var update_profile_and_clear_task = function (err, data, reject, resolve) {
+  if (err) {
+    console.log("Error updating " + err);
+    reject(err);
+  } else {
+    console.log("Successfully updated. Resolving task. " + JSON.stringify(data));
+    resolve(data);
+    ref.child('logs/profileUpdates').push(data); //, function (err) {if (err){ } else {}});    
+  }
+}
+
 var update_achievements_and_clear_queue = function (location, theData, data, reject, resolve) {
   // update the profile
   ref.child(location).set(theData, function (err) {
-    if (err) {
-      reject(err);
-    } else {
-      resolve(data);
-      ref.child('profileUpdateLog').push(data); //, function (err) {if (err){ } else {}});    
-    }
+    update_profile_and_clear_task(err, data, reject, resolve);
   });
 }
 
-var fetch_service_url = function (theUrl, data,service, reject, resolve) {
-  request(theUrl, function (error, response, body) {
-    console.log("requested url " + theUrl + " since the service is " + service);
-    if (!error && response.statusCode == 200) {
+var fetch_service_url = function (theUrl, data, service, reject, resolve, error, response, body, callback) {
+  //console.log("requested url " + theUrl + " since the service is " + service);
+  if (!error && response.statusCode == 200) {
 
-      var totalAchievements = get_achievements_from_response(service, body);
+    var totalAchievements = get_achievements_from_response(service, body);
+    data.count = totalAchievements;
+    var location = "classMentors/userAchievements/" + data.id + "/services/" + service;
+    var theData = { "totalAchievements": data.count, "id": data.id };
 
-      data.count = totalAchievements;
-      var tempAuth = data.id;
-      var location = "classMentors/userAchievements/" + tempAuth + "/services/" + service;
-      var theData = { "totalAchievements": data.count, "id": data.id };
+    callback(location, theData, data, reject, resolve);
+  }
+  else {
+    console.log("There was an error fetching " + theUrl + " status code " + response.statusCode + " error " + error);
+  }
+}
 
-      update_achievements_and_clear_queue(location, theData, data, reject, resolve);
+var get_profile = function (service_response_body, task_data, reject, resolve) {
+  var jsonObject = JSON.parse(service_response_body);
+  var service = task_data.service;
+  var services = jsonObject['services']
+  //console.log(services);
+  // If the user does not have the service it will be skipped. 
+  if (services.hasOwnProperty(service)) {
+    var serviceID = services[service]['details']['id'];
+
+    var theUrl = get_service_url(service, serviceID);
+    // Reject bad requests
+    if (theUrl == "") {
+      reject("Non-supported service " + service);
     }
-    else{
-      console.log("There was an error fetching "+theUrl+" status code "+response.statusCode+ " error "+error);
-    }
+    //Fetch the service url
+    request(theUrl, function (error, response, body) {
+      fetch_service_url(theUrl, task_data, service, reject, resolve, error, response, body, update_achievements_and_clear_queue);
+    });
+
+  }
+}
+
+// Called by Queue when new tasks are present. 
+var process_task = function (data, progress, resolve, reject) {
+  //console.log("service " + data.service + " for user " + data.id);
+  var service = data.service;
+  var user = data.id;
+
+  //Fetch the userProfile from ClassMentors
+  var userProfileUrl = "https://verifier.firebaseio.com/classMentors/userProfiles/" + user + ".json";
+  request(userProfileUrl, function (error, response, body) {
+    //TODO: handle profiel fetch error. 
+    //TODO: check that response is valid. 
+    //TODO: If valid, process profile. 
+    get_profile(body, data, reject, resolve);
+
   });
-}        
+}
 
 // Do not run the server when loading as a module. 
 if (require.main === module) {
-
-  var queue = new Queue(queueRef, function (data, progress, resolve, reject) {
-    console.log("service " + data.service + " for user " + data.id);
-    var service = data.service;
-    var user = data.id;
-
-    //Fetch the userProfile from ClassMentors
-    var userProfileUrl = "https://verifier.firebaseio.com/classMentors/userProfiles/" + user + ".json";
-    request(userProfileUrl, function (error, response, body) {
-      var jsonObject = JSON.parse(body);
-      var services = jsonObject['services']
-      //console.log(services);
-      // If the user does not have the service it will be skipped. 
-      if (services.hasOwnProperty(service)) {
-        var serviceID = services[service]['details']['id'];
-
-        var theUrl = get_service_url(service, serviceID);
-
-        // Reject bad requests
-        if (theUrl == "") {
-          reject("Non-supported service " + service);
-        }
-
-        //Fetch the service url
-        fetch_service_url(theUrl, data, service, reject, resolve);
-      }
-    });
-  });
+  var queue = new Queue(queueRef, process_task);
 
   // Export modules if we aren't running the worker so that we can test functions. 
 } else {
 
   module.exports = {
-    "say_hello": say_hello,
-    "say_goodbye": say_goodbye,
     "get_service_url": get_service_url,
     "get_achievements_from_response": get_achievements_from_response,
     "update_achievements_and_clear_queue": update_achievements_and_clear_queue,
-    "fetch_service_url":fetch_service_url
+    "fetch_service_url": fetch_service_url,
+    "process_task": process_task,
+    "get_profile": get_profile,
+    "update_profile_and_clear_task": update_profile_and_clear_task
   }
-
 }
 
--- test/worker.spec.js
+++ test/worker.spec.js
@@ -15,17 +15,7 @@ var worker = proxyquire("../src/worker", {
 //mock.flush();
 // data is logged
 
-
 describe("Worker", function() {
-    it("says hello", function() {
-        expect(1+1).to.equal(2);
-        expect(worker.say_hello("Chris")).to.equal("Hello Chris");
-        expect(worker.say_hello("Tom")).to.equal("Hello Tom"); 
-    });
-    it("says goodbye", function() {
-        expect(worker.say_goodbye("Chris")).to.equal("Goodbye Chris");
-        expect(worker.say_goodbye("Tom")).to.equal("Goodbye Tom"); 
-    });
     it("gets service url", function() {
         expect(worker.get_service_url("codeCombat", "chris")).to.equal("http://codecombat.com/db/user/chris/level.sessions?project=state.complete,levelID,levelName");
         expect(worker.get_service_url("codeSchool", "chris")).to.equal("https://www.codeschool.com/users/chris.json");
@@ -40,21 +30,52 @@ describe("Worker", function() {
         expect(worker.get_achievements_from_response("codeSchool", '{"badges":[]}')).to.equal(0);
     });
     
+    //Todo: Add tests
     it("update achievements", function() {
         var location = "classMentors/userAchievements/chris/services/codeCombat";
         //update_achievements_and_clear_queue(location, theData, data, reject, resolve);
         worker.update_achievements_and_clear_queue(location, {}, {}, function (data){}, function (data){});
-
     });
     
-     it("fetch service responses", function() {
-        //fetch_service_url(theUrl, data,service, reject, resolve)
-        //mock request before running this. 
-        //worker.fetch_service_url("http://home.com", {},"codeCombat", function (data){}, function (data){});  
+    //Todo: Add tests
+    it("processes tasks", function () {
+        //process_task(data, progress, resolve, reject);
+        worker.process_task({ "service": "codeCombat", "id": "Chris" }, function (data) { }, function (data) { }, function (data) { });
     });
+    
+    //Todo: Add tests
+    it("get profile", function () {
+        //get_profile(body, data, reject, resolve) ;
+        var body = JSON.stringify({"services":{"codeCombat":{"details":{"id":"Chris"}}}});
+        worker.get_profile( body, {"service":"codeCombat"}, function (data) { }, function (data) { });
+    });
+    // Need to pass in done to test that callbacks are executed.
+     it('can fetch a bad service url', function () {
+        //fetch_service_url(theUrl, data,service, reject, resolve, error,response, body,success)
+        worker.fetch_service_url("http://TESTURL.COM", {}, "codeCombat", function (data) { }, function (data) { }, false, {}, "TESTING", function(){});
+    }); 
+    it('can fetch a service profile', function (done) {
+        //fetch_service_url(theUrl, data,service, reject, resolve, error,response, body,success)
+        var success = function(){
+            expect(arguments[0]).to.equal("classMentors/userAchievements/undefined/services/codeCombat");
+            expect(arguments[1]["totalAchievements"]).to.equal(1);
+            expect(arguments[2]['count']).to.equal(1);            
+            done();
+        };
+        worker.fetch_service_url("http://TESTURL.COM", {}, "codeCombat", function (data) { }, function (data) { }, false, { "statusCode": 200 }, '[{"state":{"complete":true}}]',success);
+    });
+    
+    it('can update profile and clear task', function () {
+        //update_profile_and_clear_task(err, data)
+        worker.update_profile_and_clear_task(true, {},function (data) { }, function (data) { });
+        worker.update_profile_and_clear_task(false, {},function (data) { }, function (data) { });
+    });
+    
     
 });
 
+// To mock request
+/*
 describe('Service requests', function(){
   before(function(done){
     sinon
@@ -63,18 +84,13 @@ describe('Service requests', function(){
     done();
   });
 
-  /*
+  //it('' ...
+  
   after(function(done){
     request.get.restore();
     done();
   });
-  */
+  
 
-  it('can get service profile', function(done){
-    worker.fetch_service_url("http://TESTURL.COM", {},"codeCombat", function (data){}, function (data){});
-    // callback for the get request is never being called. 
-    // How do you properly include the done(): after the test has completed? 
-    done();
-   
-  });
 });
+*/
